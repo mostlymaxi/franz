ee3d242fa48eb4610b59448bdb5d08aed174e6f1
Max Saparov <max.saparov@gmail.com>
2024-09-20 18:19:20 -0400

    changed: many things

--- src/server.rs
+++ src/server.rs
@@ -1,13 +1,10 @@
-use disk_ringbuffer::ringbuf;
+use disk_ringbuffer::ringbuf::{self, DiskRing};
 use num_derive::FromPrimitive;
-use std::collections::HashMap;
-use std::io;
+use std::fs;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use std::path::PathBuf;
-use std::sync::{Arc, RwLock};
-use std::time::Duration;
+use std::path::{Path, PathBuf};
 
-use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter};
+use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::watch;
 use tokio::{select, signal};
@@ -17,8 +14,15 @@ pub struct FranzServer {
     server_path: PathBuf,
     sock_addr: SocketAddr,
     stop_rx: watch::Receiver<()>,
-    default_max_pages: usize,
-    topics: Arc<RwLock<HashMap<String, (ringbuf::Writer, ringbuf::Reader)>>>,
+    _default_max_pages: usize,
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum FranzError {
+    #[error(transparent)]
+    RingbufError(#[from] ringbuf::RingbufError),
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
 }
 
 #[repr(u8)]
@@ -32,7 +36,7 @@ enum ClientKind {
 impl FranzServer {
     pub const DEFAULT_IPV4: (u8, u8, u8, u8) = (127, 0, 0, 1);
 
-    pub fn new(server_path: PathBuf, port: u16, default_max_pages: usize) -> FranzServer {
+    pub fn new(server_path: PathBuf, port: u16, _default_max_pages: usize) -> FranzServer {
         let (stop_tx, stop_rx) = watch::channel(());
 
         tokio::spawn(async move {
@@ -43,81 +47,78 @@ impl FranzServer {
         let (a, b, c, d) = Self::DEFAULT_IPV4;
 
         let sock_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(a, b, c, d)), port);
-        let topics = Arc::new(RwLock::new(HashMap::new()));
 
         FranzServer {
             sock_addr,
-            default_max_pages,
+            _default_max_pages,
             server_path,
-            topics,
             stop_rx,
         }
     }
 
-    async fn get_or_create_topic(&self, topic: String) -> (ringbuf::Writer, ringbuf::Reader) {
-        let topics = self.topics.read().unwrap();
-        match topics.get(&topic) {
-            Some((tx, rx)) => (tx.clone(), rx.clone()),
-            None => {
-                // unlock topics RWLock
-                drop(topics);
+    async fn handle_produce<P: AsRef<Path>>(
+        &self,
+        sock: TcpStream,
+        addr: SocketAddr,
+        topic: P,
+    ) -> Result<(), FranzError> {
+        fs::create_dir_all(self.server_path.join(&topic))?;
+        let mut tx = DiskRing::<ringbuf::Sender>::new(self.server_path.join(topic))?;
 
-                let (mut tx, mut rx) =
-                    ringbuf::new(self.server_path.join(&topic), self.default_max_pages).unwrap();
-
-                tx.super_unsafe_page_cleanup_never_call_this_unless_you_know_what_youre_doing();
-                rx.super_unsafe_page_cleanup_never_call_this_unless_you_know_what_youre_doing();
-
-                let tx_c = tx.clone();
-                let rx_c = rx.clone();
-
-                let mut topics = self.topics.write().unwrap();
-
-                topics.insert(topic, (tx, rx));
-                (tx_c, rx_c)
-            }
-        }
-    }
-
-    async fn handle_produce(&self, sock: TcpStream, addr: SocketAddr, topic: String) {
-        let (mut tx, _) = self.get_or_create_topic(topic).await;
+        log::info!("({}) accepted producer", &addr);
 
         tokio::spawn(async move {
             let stream = BufReader::new(sock);
             let mut stream_lines = stream.lines();
-            while let Some(line) = stream_lines.next_line().await.unwrap() {
-                tx.push(line).unwrap();
+            while let Some(line) = stream_lines.next_line().await? {
+                tx.push(line)?;
             }
 
             log::info!("({}) disconnected", &addr);
+            Ok::<(), FranzError>(())
         });
+
+        Ok(())
     }
 
-    async fn handle_consume(&self, sock: TcpStream, addr: SocketAddr, topic: String) {
-        let (_, rx) = self.get_or_create_topic(topic).await;
+    async fn handle_consume(
+        &self,
+        mut sock: TcpStream,
+        addr: SocketAddr,
+        topic: String,
+    ) -> Result<(), FranzError> {
+        fs::create_dir_all(self.server_path.join(&topic))?;
+        let rx = DiskRing::<ringbuf::Receiver>::new(self.server_path.join(topic))?;
 
-        let mut stream = BufWriter::new(sock);
+        log::info!("({}) accepted consumer", &addr);
+
         tokio::spawn(async move {
+            let mut poll = String::new();
+            let (sock_rdr, sock_wtr) = sock.split();
+            let mut sock_rdr = BufReader::new(sock_rdr);
+            let mut sock_wtr = BufWriter::new(sock_wtr);
+
             // can make BufWriter if needed
             for msg in rx {
-                match msg {
-                    Err(e) => {
-                        log::error!("{e}");
-                        tokio::time::sleep(Duration::from_millis(10)).await;
+                match msg? {
+                    None => {
+                        sock_rdr.read_line(&mut poll).await?;
+                        poll.clear();
                     }
-                    Ok(None) => {
-                        tokio::time::sleep(Duration::from_millis(1)).await;
-                    }
-                    Ok(Some(msg)) => {
+                    Some(msg) => {
                         log::trace!("{msg}");
-                        stream.write_all(msg.as_bytes()).await.unwrap();
-                        stream.write_all(&[b'\n']).await.unwrap();
+                        sock_wtr.write_all(msg.as_bytes()).await?;
+                        sock_wtr.write_all(&[b'\n']).await?;
+                        sock_wtr.flush().await?;
                     }
                 }
             }
 
             log::info!("({}) disconnected", &addr);
+            Ok::<(), FranzError>(())
         });
+
+        Ok(())
     }
 
     async fn client_handler(&self) -> Result<(), io::Error> {
@@ -135,31 +136,45 @@ impl FranzServer {
             let (sock_rdr, _) = socket.split();
             let mut sock_rdr = BufReader::new(sock_rdr);
 
-            log::info!("({}) accepted a producer client", &addr);
+            log::info!("({}) new client", &addr);
 
-            // this is the efficient version:
-            //let _msg_length = socket.read_u32().await.unwrap();
-            //let msg_what: MsgWhat =
-            //    num::FromPrimitive::from_u8(socket.read_u8().await.unwrap()).unwrap();
-            //let topic_length = socket.read_u32().await.unwrap();
-            //let mut buf = vec![0; topic_length as usize];
-            //socket.read_exact(&mut buf).await.unwrap();
-            //let topic = String::from_utf8(buf).unwrap();
-
+            // "0" => PRODUCE
+            // "1" => CONSUME
             let mut client_kind = String::new();
-            sock_rdr.read_line(&mut client_kind).await.unwrap();
-            let client_kind = client_kind.trim().parse::<u8>().unwrap();
-            let client_kind = num::FromPrimitive::from_u8(client_kind).unwrap();
+            if let Err(e) = sock_rdr.read_line(&mut client_kind).await {
+                log::error!("{e}");
+                continue;
+            }
+            let client_kind = match client_kind.trim().parse::<u8>() {
+                Ok(c) => c,
+                Err(e) => {
+                    log::error!("{e}");
+                    continue;
+                }
+            };
+            let client_kind = match num::FromPrimitive::from_u8(client_kind) {
+                Some(c) => c,
+                None => {
+                    log::error!("failed to parse client kind {client_kind}");
+                    continue;
+                }
+            };
+            // \n
 
+            // topic_name
             let mut topic = String::new();
-            sock_rdr.read_line(&mut topic).await.unwrap();
+            if let Err(e) = sock_rdr.read_line(&mut topic).await {
+                log::error!("{e}");
+                continue;
+            }
             let topic = topic.trim().to_string();
+            // \n
 
-            match client_kind {
+            let _ = match client_kind {
                 ClientKind::Produce => self.handle_produce(socket, addr, topic).await,
                 ClientKind::Consume => self.handle_consume(socket, addr, topic).await,
                 ClientKind::Info => unreachable!(),
-            }
+            };
         }
     }
 
